88455f816207b7d7987ab30f6f0360d294b9dafa
QuinnAtSchool <ewalke33@stumail.jccc.edu>
2025-10-12 14:47:50 -0500

    Add files via upload

---
+++ me.js
@@ -0,0 +1,27 @@
+//#First assignment for Javascript class 
+//#Todays date is 10/12/2025. Is due 10/14/2025.  Quinn Walker 
+//#Assignment to create a list of varibles about myself and "print" in the debuger.
+//START SCRIPT
+
+
+//Start Declaration 
+let name_ = "Quinn Walker"; /*#Name was marked by the editor as deprecated.
+assumed name was used elsewhere for javascript. Changed var name to avoid
+conflicts. Google searching it says name has a special use in browsers. */
+let favoriteColor = "Purple";
+let hobby = "Game Design";
+let favoriteFood = "Pasta";
+let favoriteAnimal = "My cat Buttons";
+//End Declaration
+
+
+//Start "Print" 
+console.log(name_);
+console.log(favoriteColor);
+console.log(hobby);
+console.log(favoriteFood);
+console.log(favoriteAnimal);
+//End "Print"
+
+
+//END SCRIPT
