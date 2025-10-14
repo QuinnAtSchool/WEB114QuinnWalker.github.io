9fbeeeca245dbe7802aa9a1fbc6d395cdc167913
QuinnAtSchool <ewalke33@stumail.jccc.edu>
2025-10-14 12:08:48 -0500

    Create fortune.js

---
+++ fortune.js
@@ -0,0 +1,22 @@
+//Quinn Walker Extra Credit for unit 1. Prints varibles into a sentince for an html document.
+//10/14/2025
+//START OF SCRIPT
+
+
+//Start Varable declaration 
+let numberOfChildren = 5;
+let partnerName = "Sarah";
+let geographicLocation = "Texas"
+let jobTitle = "Doctor"
+let salary = 50000;
+//End Varible Declaration 
+
+
+//Start Output to screen
+let stringSentance = "You will be a " + jobTitle + " in " + geographicLocation + ",
+ and married to " + partnerName + " with " + numberOfChildren + " kids with a salary of " + salary +".";
+console.log(stringSentance)
+//End Output to screen
+
+
+//END OF SCRIPT
